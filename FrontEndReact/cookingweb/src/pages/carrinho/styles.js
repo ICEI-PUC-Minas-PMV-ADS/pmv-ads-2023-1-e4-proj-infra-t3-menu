9b13bc840cadb9a4f6ec1f275cc9db9477aa70f1
Rafael Mautone <rafaelmautone@live.com>
2023-06-22 18:17:09 -0300

    correcao da responsividade do Carrinho

--- FrontEndReact/cookingweb/src/pages/carrinho/styles.js
+++ FrontEndReact/cookingweb/src/pages/carrinho/styles.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 
 export const ProductsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 24px;
-  margin: 32px`;
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+grid-gap: 24px;
+margin: 32px;
+
+@media (max-width: 1200px) {
+  grid-template-columns: repeat(4, 1fr);
+}
+
+@media (max-width: 992px) {
+  grid-template-columns: repeat(3, 1fr);
+}
+
+@media (max-width: 768px) {
+  grid-template-columns: repeat(2, 1fr);
+}
+
+@media (max-width: 576px) {
+  grid-template-columns: 1fr;
+}
+`;
 
 export const Product = styled.div`
   display: flex;
